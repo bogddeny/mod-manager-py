854a5234592eaae45fb04e6620c013ad996ad11e
Bogdan Denysenko <bogddeny@gmail.com>
2023-07-21 18:30:16 +0300

    clean up main from old tests

--- main.py
+++ main.py
@@ -1,24 +1,14 @@
 import sys
-import time
 
 from PySide6.QtWidgets import QApplication
 
-import filesystem
 import window
 
 LOG_DEBUG = True
-MODS_DIR = "test/mods"
-GAME_DIR = "test/game"
-MOUNT_DIR = "test/mount"
-PRIORITY_FILE = "profile/mods_priority.txt"
-
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     win = window.MainWindow()
     win.show()
-    app.aboutToQuit.connect(print("handle about to quit later"));
+    app.aboutToQuit.connect(print("handle about to quit later"))
     sys.exit(app.exec())
-    #filesystem.mount_mods(MODS_DIR, GAME_DIR, MOUNT_DIR, PRIORITY_FILE)
-    #time.sleep(30)
-    #filesystem.unmount_mods(MOUNT_DIR)
